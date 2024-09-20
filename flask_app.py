a52ef923e41ab9ac5c2c70b2a312ae2d2641914f
Romulo Balsalobre <romulobalsalobre@gmail.com>
2024-09-20 00:22:57 +0000

    teste

--- flask_app.py
+++ flask_app.py
@@ -1,12 +1,29 @@
 
 # A very simple Flask Hello World app for you to get started with...
 
-from flask import Flask, render_template
+from flask import Flask, redirect, render_template, request, url_for
+comments = []
+
 
 app = Flask(__name__)
 app.config["DEBUG"] = True
 
+SQLALCHEMY_DATABASE_URI = "mysql+mysqlconnector://{username}:{password}@{hostname}/{databasename}".format(
+    username="RomuloBalsalobre",
+    password="131297",
+    hostname="RomuloBalsalobre.mysql.pythonanywhere-services.com",
+    databasename="RomuloBalsalobre$reports",
+)
+app.config["SQLALCHEMY_DATABASE_URI"] = SQLALCHEMY_DATABASE_URI
+app.config["SQLALCHEMY_POOL_RECYCLE"] = 299
+app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
 
-@app.route("/")
+
+
+@app.route("/", methods=["GET", "POST"])
 def index():
-    return render_template("main_page.html")
+    if request.method == "GET":
+        return render_template("main_page.html", comments=comments)
+
+    comments.append(request.form["contents"])
+    return redirect(url_for('index'))
